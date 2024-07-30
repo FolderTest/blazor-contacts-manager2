ff1f2c1ba5b08d870403cad17477edb4c8be8da0
user <>
2024-07-30 11:37:18 +0200

    1

---
+++ ContactsManager/ContactsManager/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactsManager.Shared.Models;
+
+namespace ContactsManager.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
